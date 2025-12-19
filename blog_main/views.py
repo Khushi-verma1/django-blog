0329b4ea2523573860f7b175493f379250a2fb6b
Khushi <kv107980@gmail.com>
2025-12-19 16:02:22 +0530

    registration and login functionality

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,8 +1,10 @@
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from blogs.models import Blog,Category
 from assignments.models import About
-# from .form import RegistrationFrom
+from .form import RegistrationFrom
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 def home (request):
     
     # categories = Category.objects.all()
@@ -29,9 +31,39 @@ def home (request):
     return render(request, 'home.html',context)
 
 
-# def register(request):
-#     form = RegistrationFrom()
-#     context ={
-#         'form':form , 
-#     }
-#     return render(request,'register.html',context)
+def register(request):
+    if request.method == 'POST':
+         form = RegistrationFrom(request.POST)
+         if form.is_valid():
+             form.save()
+             return redirect('register')
+         else:
+             print(form.errors)
+    else:
+        form = RegistrationFrom()
+    context ={
+        'form':form , 
+    }
+    return render(request,'register.html',context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request , request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username'],
+            password = form.cleaned_data['password']   
+    
+            user = auth.authenticate(username=username,password=password)
+            if user is not None:
+                auth.login(request,user)
+            return redirect("home")
+    form = AuthenticationForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'login.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
